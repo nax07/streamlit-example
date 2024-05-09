8fe6f973ea04d760bbab81ab6c41c2ec93d8aa41
nax07 <133920749+nax07@users.noreply.github.com>
2024-05-09 02:49:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,65 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.chains.summarize import load_summarize_chain
+from langchain import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# this function is responsible for splitting the data into smaller chunks and convert the data in document format
+def chunks_and_document(txt):
+    
+    text_splitter = CharacterTextSplitter() # text splitter method by default it has chunk_size = 200 and chunk_overlap = 200
+    texts = text_splitter.split_text(txt) # split the text into smaller chunks
+    docs = [Document(page_content=t) for t in texts] # convert the splitted chunks into document format
+    
+    return docs
+    
+# Loading the Llama 2's LLM
+def load_llm():
+    # We instantiate the callback with a streaming stdout handler
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])   
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+    # loading the LLM model
+    # This open source model can be downloaded from here
+    # Their are multiple models available just replace it in place of model and try it.
+    llm = CTransformers(
+        model=r"C:\Users\venky\OneDrive\Desktop\Learnings\Projects\Llama-2-Streamlit-Chatbot\llama-2-7b-chat.ggmlv3.q2_K.bin",
+        model_type="llama",
+         max_new_tokens = 512,
+        temperature = 0.5   )
+        
+    return llm
+ 
+# this functions is used for applying the llm model with our document 
+def chains_and_response(docs):
+    
+    llm = load_llm()
+    chain = load_summarize_chain(llm,chain_type='map_reduce')
+    
+    return chain.run(docs)
+    
+# Page title
+st.set_page_config(page_title='Text Summarization App')
+st.title(' Text Summarization App')
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# Text input
+txt_input = st.text_area('Enter your text', '', height=200)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Form to accept user's text input for summarization
+result = []
+with st.form('summarize_form', clear_on_submit=True):
+    submitted = st.form_submit_button('Submit')
+    #if submitted and openai_api_key.startswith('sk-'):
+    if submitted:
+        with st.spinner('Calculating...'):
+            docs = chunks_and_document(txt_input)
+            response = chains_and_response(docs)
+            result.append(response)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if len(result):
+    st.title('Summarization Result')
+    st.info(response)
